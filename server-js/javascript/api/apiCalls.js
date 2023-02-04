cb0b455c47a72bd8778e8dc6e84cc1a2866869a9
Luis Andres Contreras <32722767+tremenditomen@users.noreply.github.com>
2023-02-04 09:54:12 -0700

    added a farm food-stand api and fromateed the data also added another api for all food services and formatted that aswell

--- server-js/javascript/api/apiCalls.js
+++ server-js/javascript/api/apiCalls.js
@@ -1,13 +1,26 @@
-
-import axios from 'axios';
+import axios from "axios";
 import express from "express";
-import { dataFormatter } from '../helpers/dataFormatter.js';
+import { dataFormatter, farmFoodFormatter , servicesFormatter } from "../helpers/dataFormatter.js";
 
 const app = express();
 axios({
-    method: 'get',
-    url: 'https://data.calgary.ca/resource/n625-9k5x.json',
-  })
-    .then((res) => {
-        dataFormatter(res.data)
-    })
+  method: "get",
+  url: "https://data.calgary.ca/resource/n625-9k5x.json",
+}).then((res) => {
+  dataFormatter(res.data);
+});
+
+axios({
+  methode: "get",
+  url: "https://data.calgary.ca/resource/ipm6-y48y.json",
+}).then((res) => {
+  farmFoodFormatter(res.data);
+});
+axios({
+  methode: "get",
+  url: "https://data.calgary.ca/resource/vdjc-pybd.json",
+}).then((res) => {
+ 
+  servicesFormatter(res.data);
+});
+
--- server-js/javascript/helpers/dataFormatter.js
+++ server-js/javascript/helpers/dataFormatter.js
@@ -2,6 +2,7 @@ export const dataFormatter = (data)=>{
     let formatedData = {}
     data.map((info)=>{
                 formatedData = {title : info["title"],
+                                adress: info["address"],
                                 host:info["host_organization"],
                                 event_type: info["event_type"],
                                 notes:info["notes"],
@@ -11,4 +12,52 @@ export const dataFormatter = (data)=>{
                                 longitude: info["longitude"],
                                 }
     })
+    // console.log(formatedData);
 }
+ export const farmFoodFormatter = (data) => {
+    let farmStandFormatted = {};
+    data.map((info) => {
+      farmStandFormatted = {
+        vendor_name: info["vendor_name"],
+        vendor_description: info["vendor_description"],
+        product_description: info["product_description"],
+        community: info["community"],
+        location: info["location_name"],
+        address: info["address"],
+        sector: info["sector"],
+        hours: info["hours"],
+        duration: info["duration"],
+        latitude: info["point"]["coordinates"][0],
+        longitude: info["point"]["coordinates"][1],
+      };
+  
+    });
+  
+  };
+  export const servicesFormatter = (res) => {
+    let formattedData = {};
+    let foodInc = []
+
+    res.map((info) => {
+      if (
+        info["licencetypes"] === "FOOD SERVICE - PREMISES (SEATING)" ||
+        info["licencetypes"] === "FOOD SERVICE - PREMISES (NO SEATING)"
+      ) {
+        formattedData = {
+          name: info["tradename"],
+          address: info["address"],
+          licentype: info["licencetypes"],
+          latitude: info["latitude"],
+          longitude: info["longitude"],
+          community: info["comdistnm"]
+        };
+      }
+      foodInc.push(formattedData)
+      const filterd = new Set([... foodInc])
+      for (const item of filterd){
+        // console.log("item~!!!!!",item);
+      }
+     
+      
+    });
+  };
