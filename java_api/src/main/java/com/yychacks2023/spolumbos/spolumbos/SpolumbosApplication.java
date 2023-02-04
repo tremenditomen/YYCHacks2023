de8afbf9e41a603275c07acaac31b6046005bd7e
Jurgen Beu <jurgen.beu@hotmail.com>
2023-02-03 19:35:26 -0700

    initial api

---
+++ java_api/src/main/java/com/yychacks2023/spolumbos/spolumbos/SpolumbosApplication.java
@@ -0,0 +1,13 @@
+package com.yychacks2023.spolumbos.spolumbos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpolumbosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpolumbosApplication.class, args);
+	}
+
+}
