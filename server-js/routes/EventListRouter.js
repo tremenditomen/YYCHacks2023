5458acef89b3384903600949318b885df187da7e
Titan <untoldtitan3k@gmail.com>
2023-02-04 18:00:47 -0700

    Changes

--- server-js/routes/EventListRouter.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { getEventList } from '../controller/Event.js';
-const router = express.Router();
-
-router.get("/", getEventList)
-
-export default router;
--- server-js/routes/EventRouter.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-// import { getEventList } from '../controller/events.js';
-const router = express.Router();
-
-// router.get("/", getEventList)
-
-export default router;
