98afccc72efdd991197b6caaf815e508580faf78
Titan <untoldtitan3k@gmail.com>
2023-02-05 00:27:15 -0700

    Wow. Lots of changes.

--- frontend_flutter/lib/home_page.dart
+++ frontend_flutter/lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
-
+import 'dart:convert';
+import "package:dio/dio.dart";
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
@@ -15,6 +16,41 @@ class _HomePageState extends State<HomePage> {
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
 
+  List<dynamic> events = [];
+
+  Set<Marker> markers = {};
+
+  @override
+  void initState() {
+    getEventData();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    Dio().close(force: true);
+    super.dispose();
+  }
+
+  void getEventData() async {
+    var response = await Dio().get("http://20.246.2.152/api1/events");
+    setState(() {
+      events = response.data;
+    });
+    Iterable eventsToBeMarked = events.getRange(0, 20);
+    eventsToBeMarked.forEach((element) {
+      markers.add(
+        Marker(
+          markerId: MarkerId(element["name"]),
+          position: LatLng(element["latitude"], element["longitude"]),
+        ),
+      );
+    });
+    setState(() {
+      markers = markers.toSet();
+    });
+  }
+
   static const CameraPosition _initalCalgary = CameraPosition(
       target: LatLng(51.02323469598919, -114.06727911115647), zoom: 10);
 
@@ -29,22 +65,81 @@ class _HomePageState extends State<HomePage> {
           onMapCreated: (GoogleMapController controller) {
             _controller.complete(controller);
           },
+          markers: markers,
         ),
         SlidingUpPanel(
           color: Colors.red[800]!,
           panel: Padding(
             padding: const EdgeInsets.only(top: 50.0),
-            child: ListView(children: const [
-              Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Card(
-                  surfaceTintColor: Colors.white,
-                  child: SizedBox(
-                    height: 125,
+            child: events.isEmpty
+                ? const Center(
+                    child: SizedBox(
+                      height: 50,
+                      width: 50,
+                      child: CircularProgressIndicator(
+                        color: Colors.white,
+                      ),
+                    ),
+                  )
+                : ListView(
+                    children: events.map<Widget>(
+                      (e) {
+                        return Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: Card(
+                              clipBehavior: Clip.hardEdge,
+                              child: SizedBox(
+                                height: 125,
+                                child: Row(
+                                  children: [
+                                    SizedBox(
+                                      width: 150,
+                                      child: Image(
+                                        fit: BoxFit.fitWidth,
+                                        image: e["img_url"] != null
+                                            ? NetworkImage(e["img_url"])
+                                            : const NetworkImage(
+                                                "https://via.placeholder.com/150"),
+                                      ),
+                                    ),
+                                    Column(
+                                      children: [
+                                        Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Text(
+                                            e["name"],
+                                            overflow: TextOverflow.fade,
+                                            style:
+                                                const TextStyle(fontSize: 20),
+                                            textAlign: TextAlign.left,
+                                          ),
+                                        ),
+                                        Text(
+                                          e["address"],
+                                          textAlign: TextAlign.left,
+                                        ),
+                                        Text(
+                                          e["phone_number"],
+                                          textAlign: TextAlign.left,
+                                        )
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ));
+                      },
+                    ).toList(),
+                    // Padding(
+                    //   padding: EdgeInsets.all(10.0),
+                    //   child: Card(
+                    //     surfaceTintColor: Colors.white,
+                    //     child: SizedBox(
+                    //       height: 125,
+                    //     ),
+                    //   ),
+                    // )
                   ),
-                ),
-              )
-            ]),
           ),
         )
       ],
--- frontend_flutter/lib/profile_page.dart
+++ frontend_flutter/lib/profile_page.dart
@@ -1,4 +1,6 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -10,32 +12,54 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   bool qrCode = false;
 
+  List<dynamic> art = [];
+  List<dynamic> events = [];
+
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
+  void getData() async {
+    var artRes = await Dio().get("http://20.246.2.152/api1/events/art");
+    var eventsRes = await Dio().get("http://20.246.2.152/api1/events");
+    setState(() {
+      art = artRes.data;
+
+      events = eventsRes.data;
+      events.removeRange(0, 15);
+    });
+    print("Got Art");
+  }
+
+  void openURL(String url) async {
+    await launchUrl(Uri.parse(url), mode: LaunchMode.externalApplication);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Padding(
-          padding: const EdgeInsets.only(top: 20.0),
-          child: Center(
-            child: SizedBox(
-              width: 200,
-              height: 200,
-              child: InkWell(
-                onTap: () {
-                  setState(() {
-                    qrCode = !qrCode;
-                  });
-                },
-                child: qrCode
-                    ? const CircleAvatar(
-                        backgroundImage: AssetImage("assets/code.png"),
-                      )
-                    : const CircleAvatar(
-                        backgroundImage: AssetImage("assets/serene.gif"),
-                      ),
-              ),
-            ), //Serene Yew lol
-          ),
+        Center(
+          child: SizedBox(
+            width: 200,
+            height: 200,
+            child: InkWell(
+              onTap: () {
+                setState(() {
+                  qrCode = !qrCode;
+                });
+              },
+              child: qrCode
+                  ? const CircleAvatar(
+                      backgroundImage: AssetImage("assets/code.png"),
+                    )
+                  : const CircleAvatar(
+                      backgroundImage: AssetImage("assets/serene.gif"),
+                    ),
+            ),
+          ), //Serene Yew lol
         ),
         const Padding(
           padding: EdgeInsets.only(top: 20.0),
@@ -44,8 +68,15 @@ class _ProfilePageState extends State<ProfilePage> {
             style: TextStyle(fontSize: 20),
           ),
         ),
+        const Text("750/1000 to Level 5"),
+        const Padding(
+          padding: EdgeInsets.all(10.0),
+          child: LinearProgressIndicator(
+            value: .7,
+          ),
+        ),
         Padding(
-          padding: const EdgeInsets.only(left: 32.5),
+          padding: const EdgeInsets.only(left: 32.5, bottom: 5),
           child: Row(
             children: const [
               Padding(
@@ -85,71 +116,116 @@ class _ProfilePageState extends State<ProfilePage> {
           height: 300,
           child: ListView(
             children: [
-              const Text("Exclusive Offers"),
-              SizedBox(
-                height: 180,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: const [
-                    Card(
-                      child: SizedBox(
-                        width: 200,
-                        height: 180,
-                      ),
-                    ),
-                    Card(
-                      child: SizedBox(
-                        width: 200,
-                        height: 180,
-                      ),
-                    ),
-                    Card(
-                      child: SizedBox(
-                        width: 200,
-                        height: 180,
-                      ),
-                    ),
-                    Card(
-                      child: SizedBox(
-                        width: 200,
-                        height: 180,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
               const Text("Saved Places"),
               SizedBox(
-                height: 180,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: const [
-                    Card(
-                      child: SizedBox(
-                        width: 200,
-                        height: 180,
+                height: 150,
+                child: art.isEmpty
+                    ? const Center(child: CircularProgressIndicator())
+                    : ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: art.map<Widget>(
+                          (e) {
+                            return Card(
+                              clipBehavior: Clip.hardEdge,
+                              child: InkWell(
+                                onTap: () => openURL(
+                                    "https://www.calgary.ca/arts-culture/public-art.html"),
+                                child: SizedBox(
+                                  width: 200,
+                                  child: Column(
+                                    children: [
+                                      Align(
+                                        alignment: Alignment.topLeft,
+                                        child: Padding(
+                                          padding: const EdgeInsets.only(
+                                              left: 8.0, top: 8),
+                                          child: Text(
+                                            e["title"],
+                                            style: TextStyle(fontSize: 20),
+                                          ),
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.only(
+                                            left: 8.0, bottom: 2),
+                                        child: Align(
+                                          alignment: Alignment.topLeft,
+                                          child: Text("By: " + e["artist"]),
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.only(
+                                            left: 8.0, bottom: 2),
+                                        child: Text(e["address"]),
+                                      ),
+                                      const Text(
+                                        "City of Calgary Public Art",
+                                        style: TextStyle(fontSize: 10),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        ).toList(),
                       ),
-                    ),
-                    Card(
-                      child: SizedBox(
-                        width: 200,
-                        height: 180,
+              ),
+              const Text("Exclusive Offers"),
+              SizedBox(
+                height: 150,
+                child: events.isEmpty
+                    ? const Center(child: CircularProgressIndicator())
+                    : ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: events.map<Widget>(
+                          (e) {
+                            return Card(
+                              clipBehavior: Clip.hardEdge,
+                              child: InkWell(
+                                onTap: () => openURL(e["url"]),
+                                child: SizedBox(
+                                  width: 300,
+                                  child: Row(
+                                    children: [
+                                      Image(
+                                        height: 150,
+                                        width: 100,
+                                        fit: BoxFit.fitHeight,
+                                        image: NetworkImage(
+                                          e["img_url"],
+                                        ),
+                                      ),
+                                      Column(
+                                        children: [
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(
+                                              e["name"],
+                                              overflow: TextOverflow.fade,
+                                              style:
+                                                  const TextStyle(fontSize: 20),
+                                              textAlign: TextAlign.left,
+                                            ),
+                                          ),
+                                          Text(
+                                            e["address"],
+                                            textAlign: TextAlign.left,
+                                          ),
+                                          Text(
+                                            e["phone_number"],
+                                            textAlign: TextAlign.left,
+                                          )
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        ).toList(),
                       ),
-                    ),
-                    Card(
-                      child: SizedBox(
-                        width: 200,
-                        height: 180,
-                      ),
-                    ),
-                    Card(
-                      child: SizedBox(
-                        width: 200,
-                        height: 180,
-                      ),
-                    ),
-                  ],
-                ),
               )
             ],
           ),
