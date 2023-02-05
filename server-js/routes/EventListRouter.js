c4b729a60c68fd09e1bac7fbf204993f07f9624f
Titan <untoldtitan3k@gmail.com>
2023-02-04 21:43:29 -0700

    I think its all fixed now

--- server-js/routes/EventListRouter.js
+++ server-js/routes/EventListRouter.js
@@ -2,6 +2,6 @@ import express from 'express';
 import { getEventList } from '../controller/Event.js';
 const router = express.Router();
 
-router.get("/events", getEventList)
+router.get("/eventList", getEventList)
 
 export default router;
