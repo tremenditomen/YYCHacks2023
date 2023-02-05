3ccc8920c98123402eedfadcd7d7d92ace341e98
Ryan Pang <RyanPang12@gmail.com>
2023-02-04 17:31:09 -0700

    Set root to /api1/

--- server-js/index.js
+++ server-js/index.js
@@ -10,7 +10,7 @@ app.use(compression());
 app.use(bodyParser.json({limit:"30mb",extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
 app.use(cors());
-app.use('/',rootRouter);
+app.use('/api1/',rootRouter);
 const CONNECTION_URL = process.env.DB;
 const port = process.env.PORT||5000;
 console.log(CONNECTION_URL);
